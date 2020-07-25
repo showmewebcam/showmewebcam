a9bb735c6caa645f3b10dea6de9c31b9e0a4fa68
Huan Truong <htruong@tnhh.net>
2020-07-25 00:07:22 -0700

    Add piwebcam package, fixup board config

--- build-showmewebcam.sh
+++ build-showmewebcam.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 cd ../buildroot
-
+BR2_EXTERNAL=../showmewebcam/ make raspberry0cam_defconfig
+make all
---
+++ package/piwebcam/piwebcam.mk
@@ -0,0 +1,23 @@
+################################################################################
+#
+# piwebcam
+#
+################################################################################
+
+PIWEBCAM_VERSION = 7f75011705c53c58e2faee9ef8bdc695fc389da8
+PIWEBCAM_SITE = git://github.com/showmewebcam/uvc-gadget.git
+PIWEBCAM_LICENSE = GPL-2.0+
+PIWEBCAM_LICENSE_FILES = LICENSE
+PIWEBCAM_DEST_DIR = /opt/uvc-webcam
+PIWEBCAM_SITE_METHOD = git
+
+define PIWEBCAM_BUILD_CMDS
+	$(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" LD="$(TARGET_LD)" -C $(@D)
+endef
+
+define PIWEBCAM_INSTALL_TARGET_CMDS
+	mkdir $(TARGET_DIR)$(PIWEBCAM_DEST_DIR)
+	$(INSTALL) -D -m 0755 $(@D)/uvc-gadget $(TARGET_DIR)$(PIWEBCAM_DEST_DIR)
+	$(INSTALL) -D -m 0755 $(@D)/multi-gadget.sh $(TARGET_DIR)$(PIWEBCAM_DEST_DIR)
+	$(INSTALL) -D -m 0755 $(@D)/start-webcam.sh $(TARGET_DIR)$(PIWEBCAM_DEST_DIR)
+endef
