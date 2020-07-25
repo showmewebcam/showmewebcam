a92eb2e7ebfee72d9892c9cbc91714cc794cd62b
Huan Truong <htruong@tnhh.net>
2020-07-24 22:40:06 -0700

    Add initial board bringup config: Dropbear, Wifi

--- board/raspberry0cam/post-build.sh
+++ board/raspberry0cam/post-build.sh
@@ -9,3 +9,8 @@ if [ -e ${TARGET_DIR}/etc/inittab ]; then
 	sed -i '/GENERIC_SERIAL/a\
 tty1::respawn:/sbin/getty -L  tty1 0 vt100 # HDMI console' ${TARGET_DIR}/etc/inittab
 fi
+
+cp package/busybox/S10mdev ${TARGET_DIR}/etc/init.d/S10mdev
+chmod 755 ${TARGET_DIR}/etc/init.d/S10mdev
+cp package/busybox/mdev.conf ${TARGET_DIR}/etc/mdev.conf
+
