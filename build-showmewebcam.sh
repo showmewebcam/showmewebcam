3bcbe30fc8d8a3badf3629f9c8272bb80b61587c
tuyenld <tuyenld.work@gmail.com>
2020-10-08 22:37:10 +0700

    Support Raspberry Pi Zero v1.3 (no Wi-Fi)
    - We need overlays partion because of setting "dtoverlay=dwc2" in the config.txt
    dwc2.dtbo will be looked for in the /boot/overlays
    (see more: part "3.1: DTBs, overlays and config.txt" https://www.raspberrypi.org/documentation/configuration/device-tree.md)
    
    - According to https://github.com/raspberrypi/firmware/blob/d46e089301ba5b2e6768c78d706e9d28370e3c1c/boot/overlays/README#L722 it read
    Name:   dwc-otg
    Info:   Selects the dwc_otg USB controller driver which has fiq support. This
            is the default on all except the Pi Zero which defaults to dwc2.
    Load:   dtoverlay=dwc-otg
    Params: <None>
    
    This is not true. If the dwc2 wasn't use, you'll have an error:
    $ journalctl --no-pager -u piwebcam
    Feb 07 15:51:04 showmepicam piwebcam[153]: UVC: device open failed: No such file or directory (2).
    Feb 07 15:51:04 showmepicam systemd[1]: piwebcam.service: Main process exited, code=exited, status=1/FAILURE
    Feb 07 15:51:04 showmepicam systemd[1]: piwebcam.service: Failed with result 'exit-code'.

--- build-showmewebcam.sh
+++ build-showmewebcam.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
+ORIGIN_PIZEROW_CAM="raspberrypi0cam"
+target_defconfig="$ORIGIN_PIZEROW_CAM"_defconfig
+
+case "$1" in
+        raspberrypi0)
+			target_defconfig="$1_defconfig"
+			target_defconfig_loc="configs/$target_defconfig"
+			# post-image.sh will gen corresponding image depend on basename path
+			ln -sfrn "board/$ORIGIN_PIZEROW_CAM" "board/$1"
+			cp -f "configs/$ORIGIN_PIZEROW_CAM"_defconfig "$target_defconfig_loc"
+
+			sed "1i ### DO NOT EDIT, this is generated file from $ORIGIN_PIZEROW_CAM defconfig" -i "$target_defconfig_loc"
+			# change path
+			sed "s/board\/raspberrypi0cam/board\/$1/g" -i "$target_defconfig_loc"
+			# change dts file
+			sed "s/bcm2708-rpi-zero-w/bcm2708-rpi-zero/g" -i "$target_defconfig_loc"
+            ;;
+        *)
+			target_defconfig="$ORIGIN_PIZEROW_CAM"_defconfig
+esac
+
 cd ../buildroot
-BR2_EXTERNAL=../showmewebcam/ make raspberry0cam_defconfig
+BR2_EXTERNAL=../showmewebcam/ make $target_defconfig
 make all
