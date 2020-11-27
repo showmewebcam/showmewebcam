162583878c3c7a7ad1050c8dd0e0ec4d2b927727
Richard Casemore <richard@massless.io>
2020-11-27 08:47:48 +0000

    Support for Raspberry Pi 4

--- build-showmewebcam.sh
+++ build-showmewebcam.sh
@@ -3,10 +3,14 @@
 ORIGIN_PI_CAM="raspberrypi0cam" # renamed from raspberrypi0w
 target_defconfig="$ORIGIN_PI_BOARD"_defconfig
 
-BOARDNAME=$1
+BOARDNAME=$1cam
 
 case "$BOARDNAME" in
-        raspberrypi0)
+        raspberrypi4cam)
+			target_defconfig="${BOARDNAME}_defconfig"
+			target_defconfig_loc="configs/$target_defconfig"
+        ;;
+        raspberrypi0cam)
 			target_defconfig="${BOARDNAME}_defconfig"
 			target_defconfig_loc="configs/$target_defconfig"
 			# post-image.sh will gen corresponding image depend on basename path
@@ -19,16 +23,16 @@ case "$BOARDNAME" in
 			# change dts file
 			sed "s/bcm2708-rpi-zero-w/bcm2708-rpi-zero/g" -i "$target_defconfig_loc"
         ;;
-        raspberrypi0w)
+        raspberrypi0wcam)
 			target_defconfig="$ORIGIN_PI_CAM"_defconfig
 
         ;;
         *)
 			echo "usage: BUILDROOT_DIR=../buildroot $0 (boardname)"
-			echo "boardname: raspberrypi0, raspberrypi0w"
+			echo "boardname: raspberrypi0, raspberrypi0w, raspberrypi4"
 			exit 1
         ;;
 esac
 
 BR2_EXTERNAL="$(pwd)" make O="$(pwd)/output/$BOARDNAME" -C "$BUILDROOT_DIR" "$target_defconfig"
-make -C "output/$BOARDNAME" all
+make -s -C "output/$BOARDNAME" all
